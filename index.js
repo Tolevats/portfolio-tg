9e5ef0b79579608e5b96bf3552435bce2fe23211
Tolevats <pstavelot@gmail.com>
2024-08-23 19:15:00 -0400

    add index.js file

---
+++ index.js
@@ -0,0 +1,26 @@
+//Aquí definir rutas e importar los componentes a renderizar.
+
+import { setRootEl, setRoutes, onURLChange } from './router.js';
+import Home from './views/Home.js';
+import Alice from './views/Alice.js';
+import Buck from './views/Buck.js';
+import T from './views/T.js';
+
+const routes = {
+    '/': Home,
+    '/alice': Alice,
+    '/buck': Buck,
+    '/t': T,
+};
+
+setRoutes(routes);
+
+window.addEventListener('DOMContentLoaded', () => {
+    setRootEl(document.getElementById('root'));
+
+    onURLChange(window.location);
+
+    window.addEventListener('popstate', () => {
+        onURLChange(window.location);
+    });
+});
