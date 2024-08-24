20a1490f9744efa709c898e5fafaf9b75581b7ec
Tolevats <pstavelot@gmail.com>
2024-08-24 02:12:20 -0400

    add working footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,11 @@
 export const Footer = () => {
     const footerEl = document.createElement('footer');
     footerEl.innerHTML = `
-    <nav></nav>
-    `
-}
+    <section class="footer">
+        <p class="author">Made by Pas with 🖤</p>
+        <p class="copy">2024 &copy;</p>
+    </section>
+    `;
+
+    return footerEl;
+};
