76cb0fb0b240dc07851742899bb882ba7c18e08d
Tolevats <pstavelot@gmail.com>
2024-08-27 22:41:21 -0400

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export const Footer = () => {
     const footerEl = document.createElement('footer');
     footerEl.innerHTML = `
     <section class="footer">
-        <p class="author">Made by Pas with 🖤</p>
+        <p class="author">Made with 🖤 by Pas</p>
         <p class="copy">2024 &copy;</p>
     </section>
     `;
