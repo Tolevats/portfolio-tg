91e9f7193e5a3d434b706ae8b4e92e8a43219a80
Tolevats <pstavelot@gmail.com>
2024-08-27 23:29:44 -0400

    update home view

--- views/Home.js
+++ views/Home.js
@@ -1,68 +1,26 @@
-/* import { Header } from "../components/Header";
-import { Footer } from "../components/Footer"; */
+import { Header } from "../components/Header.js";
+//import { Nav } from "../components/Nav.js";
+import { Footer } from "../components/Footer.js";
 
 const Home = () => {
     const mainEl = document.createElement('main');
-    mainEl.innerHTML =
-        `
-    <h1>Tara Gregson</h1>
-    <form class="row g-3 needs-validation" novalidate>
-        <div class="col-md-4 position-relative">
-            <label for="validationTooltip01" class="form-label">First name</label>
-            <input type="text" class="form-control" id="validationTooltip01" value="Mark" required>
-            <div class="valid-tooltip">
-                Looks good!
-            </div>
-        </div>
-        <div class="col-md-4 position-relative">
-            <label for="validationTooltip02" class="form-label">Last name</label>
-            <input type="text" class="form-control" id="validationTooltip02" value="Otto" required>
-            <div class="valid-tooltip">
-                Looks good!
-            </div>
-        </div>
-        <div class="col-md-4 position-relative">
-            <label for="validationTooltipUsername" class="form-label">Username</label>
-            <div class="input-group has-validation">
-                <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
-                <input type="text" class="form-control" id="validationTooltipUsername" aria-describedby="validationTooltipUsernamePrepend" required>
-                <div class="invalid-tooltip">
-                    Please choose a unique and valid username.
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 position-relative">
-            <label for="validationTooltip03" class="form-label">City</label>
-            <input type="text" class="form-control" id="validationTooltip03" required>
-            <div class="invalid-tooltip">
-                Please provide a valid city.
-            </div>
-        </div>
-        <div class="col-md-3 position-relative">
-            <label for="validationTooltip04" class="form-label">State</label>
-            <select class="form-select" id="validationTooltip04" required>
-                <option selected disabled value="">Choose...</option>
-                <option>...</option>
-            </select>
-            <div class="invalid-tooltip">
-                Please select a valid state.
-            </div>
-        </div>
-        <div class="col-md-3 position-relative">
-            <label for="validationTooltip05" class="form-label">Zip</label>
-            <input type="text" class="form-control" id="validationTooltip05" required>
-            <div class="invalid-tooltip">
-                Please provide a valid zip.
-            </div>
-        </div>
-        <div class="col-12">
-            <button class="btn btn-primary" type="submit">Submit form</button>
-        </div>
-    </form>
+    mainEl.innerHTML = `
+    <section>
+        <h2>Recent Projects</h2>
+    </section>
     `
+//en section debo poner una galería tipo cards para exhibir proyectos 
+
+    // HEADER
+    mainEl.prepend(Header());
+
+    // NAV
+   // mainEl.appendChild(Nav());
+
+    //FOOTER
+    mainEl.appendChild(Footer());
+
     return mainEl;
 };
-
-
 
 export default Home;
