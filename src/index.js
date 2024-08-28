9560c0e062d956bc45db65c547ffca68030a447c
Tolevats <pstavelot@gmail.com>
2024-08-28 00:32:14 -0400

    fix server, add src folder

---
+++ src/index.js
@@ -0,0 +1,32 @@
+//Aquí definir rutas e importar los componentes a renderizar.
+
+import { setRootEl, setRoutes, onURLChange } from '../router.js';
+import Home from './views/Home.js';
+import Alice from './views/Alice.js';
+import Buck from './views/Buck.js';
+import T from './views/T.js';
+import About from './views/About.js';
+import Contact from './views/Contact.js';
+import NotFound from './views/NotFound.js';
+
+const routes = {
+    '/': Home,
+    '/about': About,
+    '/contact': Contact,
+    '/alice': Alice,
+    '/buck': Buck,
+    '/t': T,
+    '/404': NotFound,
+};
+
+setRoutes(routes);
+
+window.addEventListener('DOMContentLoaded', () => {
+    setRootEl(document.getElementById('root'));
+
+    onURLChange(window.location);
+});
+
+window.addEventListener('popstate', () => {
+    onURLChange(window.location);
+});
