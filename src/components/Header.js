51e58f72c1f9dbaa11705bd950f3dba3c528c1b5
Tolevats <pstavelot@gmail.com>
2024-08-29 12:58:57 -0400

    change title header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ export const Header = () => {
     const headerEl = document.createElement('header');
     headerEl.innerHTML = `
     <header>
-        <h1>Tara Gregson's Portfolio</h1>
+        <h1>Tara Gregson</h1>
     </header>
     `;
 
     return headerEl;
 }
 
-//faltaría agregar un logo
+//faltaría agregar un logo o ícono al costado derecho
