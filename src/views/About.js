0de879dc9782a7a4ce61d421fe59f76e3766c4a0
Tolevats <pstavelot@gmail.com>
2024-08-29 12:18:13 -0400

    update about view

--- src/views/About.js
+++ src/views/About.js
@@ -1,10 +1,17 @@
 import { Header } from "../components/Header.js";
-import { Nav } from "../components/Nav.js";
+//import { Nav } from "../components/Nav.js";
 import { Footer } from "../components/Footer.js";
 
 const About = () => {
     const mainEl = document.createElement('main');
     mainEl.innerHTML = `
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/home"> Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">About</li>
+            <li class="breadcrumb-item"><a href="/contact">Contact</a></li>
+        </ol>
+    </nav>
     <section>
         <h2>About me</h2>
         <p>Hi, I’m Tara Gregson—artist, wife, mother, and a woman navigating the beautiful chaos of life with Dissociative Identity Disorder. My art is an extension of me, a canvas where my different selves can meet, express, and, at times, clash. It’s a journey, sometimes a battle, as I strive to bring harmony to the many facets of my identity. I’ve learned that my alters aren’t just fragments of who I am; they are pieces of a complex mosaic that make me whole. My work reflects this ongoing exploration of self, capturing the moments where light meets shadow, and where my mind, in all its forms, finds a voice. This isn’t just my story—it’s ours, painted with the colors of resilience, love, and, yes, a fair share of struggles. Welcome to my world.</p>
@@ -35,8 +42,8 @@ const About = () => {
     // HEADER
     mainEl.prepend(Header());
 
- // NAV
-// mainEl.appendChild(Nav());
+ // NAV bootom
+    //mainEl.appendChild(Nav());
 
     //FOOTER
     mainEl.appendChild(Footer());
