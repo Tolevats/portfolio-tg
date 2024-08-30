b9f2175a91d8fe8ec06172ff6b552f8f53a42f0c
Tolevats <pstavelot@gmail.com>
2024-08-30 00:34:38 -0400

    add punctuation

--- src/views/About.js
+++ src/views/About.js
@@ -18,7 +18,7 @@ const About = () => {
             <img src="https://image.tmdb.org/t/p/original/p7MyqQ8YEwp3XfnpgrEEmAConmN.jpg" alt="Tara">
         </picture>
         <p>Hi, I’m Tara Gregson—artist, wife, mother, and a woman navigating the beautiful chaos of life with Dissociative Identity Disorder. My art is an extension of me, a canvas where my different selves can meet, express, and, at times, clash. It’s a journey, sometimes a battle, as I strive to bring harmony to the many facets of my identity. I’ve learned that my alters aren’t just fragments of who I am; they are pieces of a complex mosaic that make me whole. My work reflects this ongoing exploration of self, capturing the moments where light meets shadow, and where my mind, in all its forms, finds a voice. This isn’t just my story—it’s ours, painted with the colors of resilience, love, and, yes, a fair share of struggles. Welcome to my world.</p>
-        <p class="Alice">I love to paint butterflies -A</p> 
+        <p class="Alice">I love to paint butterflies. -A</p> 
         <p class="T">I like to paint dark, abstract shapes. -T</p>
         <p class="Buck">I just don't understand art. -B</p>
     </section>
