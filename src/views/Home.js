4ae1eca75a7bd5e787f8870bf76c109f15407862
Tolevats <pstavelot@gmail.com>
2024-08-30 01:11:47 -0400

    update home view

--- src/views/Home.js
+++ src/views/Home.js
@@ -17,10 +17,45 @@ const Home = () => {
         <h2>Recent Projects</h2>
     </header>
     
-    <section>
-        <picture>
-            <img src="/src/assets/imgs/tara-mural.jpg">
-        </picture>
+    <section class="projects">
+      <div id="carouselCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://img3.hulu.com/user/v3/artwork/3020c930-01a7-4b54-8db1-2a8eea653247?base_image_bucket_name=image_manager&base_image=c588529c-1640-4a28-a5d9-5f37b2514c4d&size=600x338&format=webp" class="d-block w-100" alt="Project 1">
+            <div class="carousel-caption d-none d-md-block">
+              <h5 class="title-caption">Project 1</h5>
+              <p class="caption">Here in the early stages of the mural</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://www.pexels.com/es-es/foto/creativo-pared-muro-mesa-7045860/" class="d-block w-100" alt="Project 2">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p class="caption">Woman.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="Project 3">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p class="caption">Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </section>
     `
 //en section debo poner una galería tipo cards para exhibir proyectos 
