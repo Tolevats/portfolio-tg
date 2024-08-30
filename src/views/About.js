fe89ea015c8cbf78cd73777a4c05cebbfa9684e4
Tolevats <pstavelot@gmail.com>
2024-08-29 22:31:52 -0400

    add style for about view

--- src/views/About.js
+++ src/views/About.js
@@ -14,28 +14,33 @@ const About = () => {
     </nav>
     <section>
         <h2>About me</h2>
+        <picture id="profile">
+            <img src="https://image.tmdb.org/t/p/original/p7MyqQ8YEwp3XfnpgrEEmAConmN.jpg" alt="Tara">
+        </picture>
         <p>Hi, I’m Tara Gregson—artist, wife, mother, and a woman navigating the beautiful chaos of life with Dissociative Identity Disorder. My art is an extension of me, a canvas where my different selves can meet, express, and, at times, clash. It’s a journey, sometimes a battle, as I strive to bring harmony to the many facets of my identity. I’ve learned that my alters aren’t just fragments of who I am; they are pieces of a complex mosaic that make me whole. My work reflects this ongoing exploration of self, capturing the moments where light meets shadow, and where my mind, in all its forms, finds a voice. This isn’t just my story—it’s ours, painted with the colors of resilience, love, and, yes, a fair share of struggles. Welcome to my world.</p>
-        <p>I'm Alice, and I love to paint butterflies. But sometimes, T takes over and paints dark, abstract shapes. It's confusing, but also kind of exciting.</p>
+        <p class="Alice">I love to paint butterflies -A</p> 
+        <p class="T">I like to paint dark, abstract shapes. -T</p>
+        <p class="Buck">I just don't understand art. -B</p>
     </section>
     <aside>
         <h3>Skills</h3>
-        <ul>
-            <div class=artistic>
-                <li>Mural painting</li>
-                <li>Portraits</li>
-                <li>Drawing</li>
-                <li>Sketching</li>
-            </div>
-            <div class="soft">
-                <li>Creative Expression</li>
-                <li>Versatility:</li>
-                <li>Resilience</li>
-                <li>Empathy and Emotional Intelligence</li>
-                <li>Collaboration</li>
-                <li>Storytelling</li>
-                <li>Adaptability</li>
-                <li>Mental Health Advocacy</li>
-            </div>
+        <ul class="artistic">
+            <li>Mural painting</li>
+            <li>Portraits</li>
+            <li>Drawing</li>
+            <li>Sketching</li>
+            <li>Interior design</li>
+        </ul>
+        <h3>Soft skills</h3>    
+        <ul class="soft">
+            <li>Creative Expression</li>
+            <li>Versatility</li>
+            <li>Resilience</li>
+            <li>Empathy and Emotional Intelligence</li>
+            <li>Collaboration</li>
+            <li>Storytelling</li>
+            <li>Adaptability</li>
+            <li>Mental Health Advocacy</li>
         </ul>
     </aside>
     `
