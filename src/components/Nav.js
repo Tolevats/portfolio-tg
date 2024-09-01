8bb3daca0cadf88b5e36000a25016197c8225923
Tolevats <pstavelot@gmail.com>
2024-09-01 00:03:54 -0400

    add html to nav element

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,30 @@ export const Nav = () => {
     const navEl = document.createElement('nav');
     navEl.innerHTML = `
     </nav>
+        <div class="container-nav">
+            <ul class="other-sections">
+                <li><a href="/services">Services</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/journal">Journal</a></li>
+            </ul>
+            <ul class="alters">
+                <li><a href="/alice">Alice</a></li>
+                <li><a href="/buck">Buck</a></li>
+                <li><a href="/t">T</a></li>
+            </ul>
+            <div class="social-links">
+                <a href="https://www.linkedin.com"><i class="fab fa-linkedin"></i></a>
+                <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+            </div>
+            <form action="newsletter.php" method="post">
+                <input type="text" name="name" placeholder="Name" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="submit" value="Subscribe">
+            </form>
+        </div>
     </nav>
-
     `
-//to-do: bottom navbar
+//to-do: add colo to container navbar 
 
     return navEl;
 }
