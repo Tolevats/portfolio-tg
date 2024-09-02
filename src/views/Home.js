79e18c5bb8d0003763c8c202d2c9ff5a1e5d57a9
Tolevats <pstavelot@gmail.com>
2024-09-02 00:09:19 -0400

    update carousel images and captions

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,7 +5,7 @@ import { Footer } from "../components/Footer.js";
 const Home = () => {
     const bodyEl = document.createElement('body');
     bodyEl.innerHTML = `
-    <nav aria-label="breadcrumb">
+    <nav class="top" aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item active" aria-current="page">Home</li>
             <li class="breadcrumb-item"><a href="/about">About</a></li>
@@ -29,29 +29,29 @@ const Home = () => {
             <img src="https://img3.hulu.com/user/v3/artwork/3020c930-01a7-4b54-8db1-2a8eea653247?base_image_bucket_name=image_manager&base_image=c588529c-1640-4a28-a5d9-5f37b2514c4d&size=600x338&format=webp" class="d-block w-100" alt="Project 1">
             <div class="carousel-caption d-none d-md-block">
               <h5 class="title-caption">Project 1</h5>
-              <p class="caption">Here in the early stages of the mural</p>
+              <p class="p-caption">In the early stages of my most recent mural</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="https://www.pexels.com/es-es/foto/creativo-pared-muro-mesa-7045860/" class="d-block w-100" alt="Project 2">
+            <img src="https://wallco.com/storage/app/resources/resize/img_8610ac27fdd55a5e11a964217dbdab41_1200_0_0_0_auto.jpeg" class="d-block w-100" alt="Project 2">
             <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p class="caption">Woman.</p>
+              <h5>Project 2</h5>
+              <p class="caption">Aren't those background leaves beautiful?</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="Project 3">
+            <img src="https://wallco.com/storage/app/resources/resize/img_de3de394ebdfc612e75e7aeb4e5ea30c_1200_0_0_0_auto.jpeg" class="d-block w-100" alt="Project 3">
             <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p class="caption">Some representative placeholder content for the third slide.</p>
+              <h5>Project 3</h5>
+              <p class="caption">Important books here.</p>
             </div>
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselCaptions" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselCaptions" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
