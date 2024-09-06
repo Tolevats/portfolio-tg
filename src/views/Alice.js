3efca982834b97a1c0b9d2ea83e93e646c062314
Tolevats <pstavelot@gmail.com>
2024-09-06 09:59:15 -0400

    add other views pictures

--- src/views/Alice.js
+++ src/views/Alice.js
@@ -1,5 +1,13 @@
 const Alice = () => {
+    const bodyEl = document.createElement('body');
+    bodyEl.innerHTML = `
+    <picture id="profile">
+        <img src="src/assets/imgs/Alice.jpg" alt="Alice">
+    </picture>
+    `
 
+    return bodyEl;
+    
 };
 
 export default Alice;
--- src/views/T.js
+++ src/views/T.js
@@ -1,5 +1,13 @@
 const T = () => {
+    const bodyEl = document.createElement('body');
+    bodyEl.innerHTML = `
+    <picture id="profile">
+        <img src="src/assets/imgs/T.jpg" alt="T">
+    </picture>
+    `
 
+    return bodyEl;
+   
 };
 
 export default T;
